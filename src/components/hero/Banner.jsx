86e0ca06d7bd2e90b850fbf0cbc2ae61a8d69c5a
abdulhay103 <abdulhay.me@gmail.com>
2024-02-27 00:06:15 +0600

    Home done

--- src/components/hero/Banner.jsx
+++ src/components/hero/Banner.jsx
@@ -6,6 +6,8 @@ import Man2 from "../../../public/hero/man-img-2.png";
 import Client1 from "../../../public/hero/client-img-1.png";
 import Client2 from "../../../public/hero/client-img-2.png";
 import Client3 from "../../../public/hero/client-img-3.png";
+import SideImage1 from "../../../public/hero/side-image-1.png";
+import arrowIcon from "../../../public/hero/arrow-icon.png";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -17,7 +19,7 @@ export default function Banner() {
       </div>
       <div className=" absolute w-full h-[890px] border-2">
         <div className=" container mx-auto h-full flex items-center justify-between ">
-          <div className="left w-full">
+          <div className="left w-full pt-14">
             <h1 className=" text-[82px] font-bold leading-[88.5px]">
               Bring your business into creativity
             </h1>
@@ -27,7 +29,7 @@ export default function Banner() {
             </p>
             <Link
               href={"/"}
-              className="px-8 py-4 bg-secondary-blue text-white font-bold text-[22px] rounded-lg"
+              className="px-8 py-4 bg-secondary-blue text-white text-[22px] rounded-lg"
             >
               Get Started
             </Link>
@@ -49,13 +51,13 @@ export default function Banner() {
               alt="side Image"
             />
             <Image
-              className=" absolute top-[320px] right-[-85px]"
+              className=" absolute top-[330px] right-[-90px]"
               src={Man2}
               width={65}
               height={65}
               alt="side-image-2"
             />
-            <div className=" absolute top-[450px] right-[-75px] flex gap-5 bg-white py-[10px] px-4 items-center rounded-xl">
+            <div className=" absolute top-[450px] right-[-80px] flex gap-5 bg-white py-[10px] px-4 items-center rounded-xl">
               <div className=" flex h-14 -space-x-5 items-center">
                 <Image src={Client1} width={55} height={55} alt="clients-1" />
                 <Image src={Client2} width={55} height={55} alt="clients-2" />
@@ -66,6 +68,20 @@ export default function Banner() {
                 <p className="">Happy Clients</p>
               </div>
             </div>
+            <Image
+              className=" absolute bottom-16 -left-20"
+              src={SideImage1}
+              alt="side image-4"
+              width={300}
+              height={205}
+            />
+            <Image
+              className=" absolute -right-28 bottom-44"
+              src={arrowIcon}
+              width={85}
+              height={120}
+              alt="arrow-icon"
+            />
           </div>
         </div>
       </div>
